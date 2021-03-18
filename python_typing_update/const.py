63aa00efdcd20aaea70fd75cbccc14b280b0db06
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-03-18 22:45:37 +0100

    Update to dev version

--- python_typing_update/const.py
+++ python_typing_update/const.py
@@ -6,8 +6,8 @@ from __future__ import annotations
 from enum import Flag, auto
 from typing import NamedTuple
 
-version = (0, 3, 0)
-dev_version = None
+version = (0, 3, 1)
+dev_version = 1
 
 version_str = '.'.join(map(str, version))
 if dev_version is not None:
