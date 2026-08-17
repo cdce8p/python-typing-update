46ca8184561adc43e8f8f4f074a42bf30b3720f6
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2026-08-17 13:08:57 +0200

    Add support for Python 3.15

--- tests/test_main.py
+++ tests/test_main.py
@@ -111,6 +111,7 @@ async def test_main(
         pytest.param(['--py312-plus']),
         pytest.param(['--py313-plus']),
         pytest.param(['--py314-plus']),
+        pytest.param(['--py315-plus']),
     ),
 )
 async def test_py_version(
